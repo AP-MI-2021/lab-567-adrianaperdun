7e38fa8a913f090e176321b54d43f81eb86caac9
adrianaperdun <91499896+adrianaperdun@users.noreply.github.com>
2021-11-01 16:01:04 +0200

    Update functions_test.py

--- Tests/functions_test.py
+++ Tests/functions_test.py
@@ -5,17 +5,17 @@ from Logic.functions import move_object, concat_str, max_price, ascending_order
 
 def test_move_object():
     lista = []
-    lista = add_object(1, "obiect 1", "descriere 1", 4000.0, "parter", lista)
-    lista = add_object(2, "obiect 2", "descriere 2", 10.0, "arhiva", lista)
-    lista = add_object(3, "obiect 3", "descriere 3", 35.2, "sala mese", lista)
-    lista = add_object(4, "obiect 4", "descriere 4", 2000.0, "birou", lista)
+    lista = add_object(1, "obiect 1", "descriere 1", 4000, "parter", lista)
+    lista = add_object(2, "obiect 2", "descriere 2", 1000, "arhiva", lista)
+    lista = add_object(3, "obiect 3", "descriere 3", 3510, "sala mese", lista)
+    lista = add_object(4, "obiect 4", "descriere 4", 2000, "birou", lista)
     lista = move_object("parter", "arhiva", lista)
     assert get_location(lista[0]) == "arhiva"
     assert get_location(lista[3]) == "arhiva"
 
 
 def test_concat_str():
-    obiect = creare_obiect(2, "Monitor Dell", "23.5'', HD, 148Hz", 1099.0, "sala conferinte")
+    obiect = creare_obiect(2, "Monitor Dell", "23.5'', HD, 148Hz", 1099, "sala conferinte")
     str = " string de test"
     obiect = concat_str(obiect, str)
     assert get_description(obiect) == "23.5'', HD, 148Hz string de test"
@@ -23,21 +23,21 @@ def test_concat_str():
 
 def test_max_price():
     lista = []
-    lista = add_object(1, "obiect 1", "descriere 1", 4000.0, "parter", lista)
-    lista = add_object(2, "obiect 2", "descriere 2", 10.0, "arhiva", lista)
-    lista = add_object(3, "obiect 3", "descriere 3", 35.2, "sala mese", lista)
-    lista = add_object(4, "obiect 4", "descriere 4", 2000.0, "birou", lista)
+    lista = add_object(1, "obiect 1", "descriere 1", 4000, "parter", lista)
+    lista = add_object(2, "obiect 2", "descriere 2", 1000, "arhiva", lista)
+    lista = add_object(3, "obiect 3", "descriere 3", 3510, "sala mese", lista)
+    lista = add_object(4, "obiect 4", "descriere 4", 2000, "birou", lista)
     rezultat = max_price(lista)
-    assert rezultat["LOC1"] == 4000.0
-    assert rezultat["LOC2"] == 3000.0
+    assert rezultat["parter"] == 4000
+    assert rezultat["arhiva"] == 3000
 
 
 def test_ascending_order():
     lista = []
-    lista = add_object(1, "obiect 1", "descriere 1", 4000.0, "parter", lista)
-    lista = add_object(2, "obiect 2", "descriere 2", 10.0, "arhiva", lista)
-    lista = add_object(3, "obiect 3", "descriere 3", 35.2, "sala mese", lista)
-    lista = add_object(4, "obiect 4", "descriere 4", 2000.0, "birou", lista)
+    lista = add_object(1, "obiect 1", "descriere 1", 4000, "parter", lista)
+    lista = add_object(2, "obiect 2", "descriere 2", 1000, "arhiva", lista)
+    lista = add_object(3, "obiect 3", "descriere 3", 3510, "sala mese", lista)
+    lista = add_object(4, "obiect 4", "descriere 4", 2000, "birou", lista)
     lista = ascending_order(lista)
     assert get_id(lista[0]) == 3
     assert get_id(lista[1]) == 4
@@ -47,10 +47,10 @@ def test_ascending_order():
 
 def test_sum():
     lista = []
-    lista = add_object(1, "obiect 1", "descriere 1", 4000.0, "parter", lista)
-    lista = add_object(2, "obiect 2", "descriere 2", 10.0, "arhiva", lista)
-    lista = add_object(3, "obiect 3", "descriere 3", 35.2, "sala mese", lista)
-    lista = add_object(4, "obiect 4", "descriere 4", 2000.0, "birou", lista)
+    lista = add_object(1, "obiect 1", "descriere 1", 4000, "parter", lista)
+    lista = add_object(2, "obiect 2", "descriere 2", 1000, "arhiva", lista)
+    lista = add_object(3, "obiect 3", "descriere 3", 3510, "sala mese", lista)
+    lista = add_object(4, "obiect 4", "descriere 4", 2000, "birou", lista)
     rezultat = sum(lista)
-    assert rezultat["parter"] == 60.0
-    assert rezultat["arhiva"] == 4000.0
+    assert rezultat["parter"] == 6000
+    assert rezultat["arhiva"] == 4000
