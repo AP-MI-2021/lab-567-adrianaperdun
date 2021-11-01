de889c7669a76d2bf5abd251bd1fddca6d1d6d29
adrianaperdun <91499896+adrianaperdun@users.noreply.github.com>
2021-11-01 15:45:28 +0200

    Update obiect.py

--- Domain/obiect.py
+++ Domain/obiect.py
@@ -9,15 +9,15 @@ def creare_obiect(id, nume, descriere, pret, locatie):
     return: un dictionar
     """
     if id < '1':
-        raise ValueError("ID-ul nu poate fi nul sau negativ!")
+        raise ValueError("ID-ul nu poate fi nul sau negativ! Introduceti alt id!")
     if len(nume) == 0:
-        raise ValueError("Numele nu poate fi nul!")
+        raise ValueError("Numele nu poate fi nul! Introduceti un nume!")
     if len(descriere) == 0:
-        raise ValueError("Descrierea nu poate fi nula!")
+        raise ValueError("Descrierea nu poate fi nula! Introducati descriere!")
     if pret.is_integer() is True and len(str(int(pret))) != 4:
-        raise ValueError("Pretul de achizitie trebuie sa aiba exact 4 cifre!")
+        raise ValueError("Pretul trebuie sa aiba exact 4 cifre! Introduceti alt pret!")
     if pret.is_integer() is False and len(str(pret).replace(".", "")) != 4:
-        raise ValueError("Pretul de achizitie trebuie sa aiba exact 4 cifre!")
+        raise ValueError("Pretul trebuie sa aiba exact 4 cifre! Introduceti alt pret!")
     if len(locatie) != 4:
         raise ValueError("Locatia trebuie sa aiba exact 4 caractere!")
     return {"id": id, "nume": nume, "descriere": descriere, "pret": pret, "locatie": locatie}
