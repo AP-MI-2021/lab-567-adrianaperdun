23cd2df967a93685fb18c189964743482ed64ee0
Andreea-Adriana Perdun <andreea.perdun@stud.ubbcluj.ro>
2021-11-01 15:55:25 +0200

    Prob. 2

--- Domain/obiect.py
+++ Domain/obiect.py
@@ -1,26 +1,24 @@
 def creare_obiect(id, nume, descriere, pret, locatie):
     """
     creeaza un dictionar ce reprezinta un obiect
-    param. id: int
+    param. id: string
     param. nume: string
     param. descriere: string
     param. pret_achizitie: float (exact 4 caractere)
     param. locatie: string (exact 4 caractere)
     return: un dictionar
     """
-    if id < '1':
+    if id < 1:
         raise ValueError("ID-ul nu poate fi nul sau negativ!")
     if len(nume) == 0:
         raise ValueError("Numele nu poate fi nul!")
     if len(descriere) == 0:
         raise ValueError("Descrierea nu poate fi nula!")
-    if pret.is_integer() is True and len(str(int(pret))) != 4:
-        raise ValueError("Pretul de achizitie trebuie sa aiba exact 4 cifre!")
-    if pret.is_integer() is False and len(str(pret).replace(".", "")) != 4:
+    if len(str(int(pret))) != 4 and len(str(pret).replace(".", "")) != 4:
         raise ValueError("Pretul de achizitie trebuie sa aiba exact 4 cifre!")
     if len(locatie) != 4:
         raise ValueError("Locatia trebuie sa aiba exact 4 caractere!")
-    return {"id": ID, "nume": nume, "descriere": descriere, "pret": pret, "locatie": locatie}
+    return {"id": id, "nume": nume, "descriere": descriere, "pret": pret, "locatie": locatie}
 
 
 def get_id(obiect):
